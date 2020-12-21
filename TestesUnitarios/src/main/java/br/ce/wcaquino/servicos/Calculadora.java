5d5b053f120e11940ecf3175ea4d699b71f13005
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2020-12-21 01:04:11 -0300

    Spy

--- TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/Calculadora.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -5,6 +5,7 @@ import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
 public class Calculadora {
 
 	public int somar(int a, int b) {
+		System.out.println("Estou executando o método somar");
 		return a + b;
 	}
 
@@ -13,14 +14,18 @@ public class Calculadora {
 	}
 
 	public int divide(int a, int b) throws NaoPodeDividirPorZeroException {
-		if(b == 0) {
+		if (b == 0) {
 			throw new NaoPodeDividirPorZeroException();
 		}
 		return a / b;
 	}
-	
+
 	public int divide(String a, String b) {
-		return Integer.valueOf(a)/Integer.valueOf(b);
+		return Integer.valueOf(a) / Integer.valueOf(b);
+	}
+
+	public void imprime() {
+		System.out.println("Passei aqui");
 	}
 
 }
