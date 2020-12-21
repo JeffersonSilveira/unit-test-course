52485a5758f93ef42f3f2697ddbb40b72491e7ea
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2020-12-21 12:31:39 -0300

    Testes em Paralelo, parte 1

--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -1,11 +1,15 @@
 package br.ce.wcaquino.servicos;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+import br.ce.wcaquino.runners.ParallelRunner;
 
+@RunWith(ParallelRunner.class)
 public class CalculadoraTest {
 
 	private Calculadora calc;
@@ -13,6 +17,12 @@ public class CalculadoraTest {
 	@Before
 	public void setup() {
 		calc = new Calculadora();
+		System.out.println("Iniciando...");
+	}
+
+	@After
+	public void tearDown() {
+		System.out.println("Finalizando...");
 	}
 
 	@Test
@@ -68,9 +78,9 @@ public class CalculadoraTest {
 	public void deveDividir() {
 		String a = "6";
 		String b = "3";
-		
+
 		int resultado = calc.divide(a, b);
-		
+
 		Assert.assertEquals(2, resultado);
 	}
 }
--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -23,12 +23,14 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -42,8 +44,10 @@ import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
 import br.ce.wcaquino.exceptions.LocadoraException;
+import br.ce.wcaquino.runners.ParallelRunner;
 import br.ce.wcaquino.utils.DataUtils;
 
+@RunWith(ParallelRunner.class)
 public class LocacaoServiceTest {
 
 	@InjectMocks @Spy
@@ -65,7 +69,14 @@ public class LocacaoServiceTest {
 	@Before
 	public void setup(){
 		MockitoAnnotations.initMocks(this);
+		System.out.println("Inciando 2...");
 	}
+	
+	@After
+	public void tearDown() {
+		System.out.println("Finalizando 2...");
+	}
+
 	
 	@Test
 	public void deveAlugarFilme() throws Exception {
